285954828a651ef99c71bee898c9bc977476fc3e
NuwanHarsha <nuwan.harshamatrix@gmail.com>
2021-05-12 23:15:08 +0530

    minor update

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 
 const productRoutes = require('./routes/products');
+const orderRoutes = require('./routes/orders');
 
-
+app.use('/orders', orderRoutes);
 app.use('/products', productRoutes);
 
 module.exports = app;
