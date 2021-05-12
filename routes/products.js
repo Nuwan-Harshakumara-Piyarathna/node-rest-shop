28b3186085395229c356c8a0ea4e26ee449d8796
NuwanHarsha <nuwan.harshamatrix@gmail.com>
2021-05-12 23:03:46 +0530

    minor update

--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,47 @@
 const express = require('express')
 const router = express.Router();
 
-router.get()
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Handle get requests'
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+    if (id == 'special') {
+        res.status(200).json({
+            message: 'You discovered special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        })
+    }
+})
+
+router.patch('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).json({
+        message: 'updated product!',
+        id: id
+    });
+})
+
+router.delete('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).json({
+        message: 'deleted product!',
+        id: id
+    });
+})
+
+router.post('/', (req, res) => {
+    res.status(200).json({
+        message: 'Handle post requests'
+    });
+});
+
+module.exports = router;
+
